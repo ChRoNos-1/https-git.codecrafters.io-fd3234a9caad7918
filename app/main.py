ffce32ea068d2d2e5ad676766b0ffdc2e88d8817
prathamesh <prathameshchauhan2004@gmail.com>
2025-02-26 23:36:40 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -3,20 +3,19 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    while True:
+        sys.stdout.write("$ ")
 
     # Wait for user input
-    command,  = input()
-    argv = command.split()
-    if argv[0] == "exit":
-        exit(int(argv[1]))
-    elif argv[0] == "echo":
-        print ("".join(argv[1:]))
-        #print(command[5::])
-    else:
-        print(f"{command}: command not found")
-    
-    main()
+        command,  = input()
+        argv = command.split()
+        if argv[0] == "exit":
+            exit(int(argv[1]))
+        elif argv[0] == "echo":
+            print ("".join(argv[1:]))
+            #print(command[5::])
+        else:
+            print(f"{command}: command not found")
     
 
 
