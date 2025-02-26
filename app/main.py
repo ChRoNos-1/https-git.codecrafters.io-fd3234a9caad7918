b681525ba5e122fff480c55f0a7b50fbd9eb5984
prathamesh <prathameshchauhan2004@gmail.com>
2025-02-26 23:34:27 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def main():
     if argv[0] == "exit":
         exit(int(argv[1]))
     elif argv[0] == "echo":
-        print (" ".join(argv[1:]))
+        print ("".join(argv[1:]))
         #print(command[5::])
     else:
         print(f"{command}: command not found")
