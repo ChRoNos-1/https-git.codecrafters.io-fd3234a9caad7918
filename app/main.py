e48e2538b85e76ad1cb965ca5844619a936f0788
prathamesh <prathameshchauhan2004@gmail.com>
2025-02-27 00:19:31 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
 import sys
 import shutil
+import subprocess
 
 def main():
     # Uncomment this block to pass the first stage
     while True:
         sys.stdout.write("$ ")
+        sys.stdout.flush()
 
     # Wait for user input
         command  = input()
         argv = command.split()
         if path := shutil.which(argv[0]):
-            cnt= len(command.split())
-            print (f"Program was passed {cnt} args (including program name)")
-            print (f"Arg #0 (program name): {argv[0]}\n")
-            print (f"Arg #1: {argv[1]}")
+            subprocess.run(argv)
+            
         if argv[0] == "type":
             if argv[1]=="exit" or argv[1]=="echo" or argv[1]=="type":
                 print (f"{argv[1]} is a shell builtin")
@@ -21,11 +21,14 @@ def main():
                 print(f"{command[5:]} is {path}")
             else:
                 print(f"{command[5:]}: not found")
+                
         elif argv[0] == "exit":
             exit(int(argv[1]))
+            
         elif argv[0] == "echo":
             print (" ".join(argv[1:]))
             #print(command[5::])
+            
         else:
             print(f"{command}: command not found")
     
