3aceb2cbf3b19eef7be7dff019e13e7aadd68ec3
prathamesh <prathameshchauhan2004@gmail.com>
2025-02-27 23:27:16 +0530

    [amy message]

--- app/main.py
+++ app/main.py
@@ -16,13 +16,18 @@ def main():
         if ">" in argv or "1>" in argv:
             if '>' in argv:
                 rin = argv.index('>')
+                
             else:
                 rin = argv.index('1>')
+                
             opfl = argv[rin + 1]
             argv = argv[:rin]
             with open (opfl,"w") as file:
                 subprocess.run(argv, stdout=file, stderr=sys.stderr)
             continue
+        
+        
+        
         elif '2>' in argv:
             rin = argv.index("2>")
             opfl = argv[rin + 1]
@@ -32,18 +37,37 @@ def main():
                 subprocess.run (argv, stderr=file)
                 continue
             
+            
+            
         elif '1>>' in argv or '>>' in argv:
             if '>>' in argv:
                 rin = argv.index('>>')
+                
             else:
                 rin = argv.index('1>>')
+                
             opfl = argv[rin + 1]
             argv = argv[:rin]
             with open (opfl,"a") as file:
                 subprocess.run(argv, stdout=file, stderr=sys.stderr)
+          
+          
+                
+        elif '2>>' in argv:
+            rin = argv.index("2>>")
+            opfl = argv[rin + 1]
+            argv = argv[:rin]
+    
+            with open (opfl,"a") as file:
+                subprocess.run (argv, stderr=file)
+                continue
+        
+        
         
         elif path := shutil.which(argv[0]):
             subprocess.run(argv)
+        
+        
             
         elif argv[0] == "cd":
             path = argv[1]
@@ -52,9 +76,13 @@ def main():
                 os.chdir(path)
             else:
                 print(f"cd: {path}: No such file or directory")
+        
+        
             
         elif argv[0]== "pwd":
             print (f"{os.getcwd()}")
+        
+        
             
         elif argv[0] == "type":
             if argv[1]=="exit" or argv[1]=="echo" or argv[1]=="type" or argv[1]=="pwd" or argv[1]=="cd" :
@@ -63,13 +91,19 @@ def main():
                 print(f"{command[5:]} is {path}")
             else:
                 print(f"{command[5:]}: not found")
+        
+        
                 
         elif argv[0] == "exit":
             exit(int(argv[1]))
+        
+        
             
         elif argv[0] == "echo":
             print (" ".join(argv[1:]))
             #print(command[7:-2:])
+        
+        
             
         else:
             print(f"{command}: command not found")
