2e129c1e3683d8092b5ce8341f97d5097261d4e9
prathamesh <prathameshchauhan2004@gmail.com>
2025-02-28 21:53:51 +0530

    [amy message]

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ def autoc (text, state):
             cn = 0
         return None
      
-     return text       
+    return text       
     #return ms[state] if state < len(ms) else None
 
 #readline.set_completion_display_matches_hook(display_matches)    
